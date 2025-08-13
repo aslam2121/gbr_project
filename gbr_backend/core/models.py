cd42531ee39ced58a2740d5d1bdc9f8375d58965
Aslam2121 <aslam2121@gmail.com>
2025-08-13 12:07:08 +0530

    Created Member model in core app and ran migrations

--- gbr_backend/core/models.py
+++ gbr_backend/core/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+class Member(models.Model):
+    username = models.CharField(max_length=150, unique=True)
+    email = models.EmailField(unique=True)
+    password = models.CharField(max_length=128)
+    category = models.CharField(max_length=50)
+    period = models.CharField(max_length=50)
+    join_date = models.DateField(auto_now_add=True)
+    expiry_date = models.DateField()
+
+    def __str__(self):
+        return self.username
